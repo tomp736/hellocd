a21d47d4a7f02c653dcb8ac1fbbb525b16b07a36
u0 <u0@localhost.localdomain>
2021-12-02 10:59:46 +0100

    change message

--- docker-mvn-build.sh
+++ docker-mvn-build.sh
@@ -1,1 +1,1 @@
-sudo docker container run -it --rm -v m2:/root/.m2 -v /home/u0/Downloads/hellocd:/app maven:alpine mvn spring-boot:run -f /app/pom.xml
+sudo docker container run -it --rm -v m2:/root/.m2 -v /home/u0/Downloads/hellocd:/app -p 8080:8080 maven:alpine mvn spring-boot:run -f /app/pom.xml
--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -1,10 +1,20 @@
 package com.example.hellocd;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class HellocdApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello Continuous Delivery!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellocdApplication.class, args);
